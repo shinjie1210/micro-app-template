e15c1154512597f96a14bb426e092a7ea7a8d625
沈兴宇 <shinjie1210@foxmail.com>
2022-08-26 15:42:34 +0800

    fix

--- base/src/config/config.js
+++ base/src/config/config.js
@@ -1,5 +1,5 @@
 
-const config = { son: 'http://localhost:4001/' }
+const config = { son: 'http://localhost:4001' }
 // 线上环境地址
 if (!/localhost/.test(window.location.href)) {
     // 基座应用和子应用部署在同一个域名下，这里使用location.origin进行补全
